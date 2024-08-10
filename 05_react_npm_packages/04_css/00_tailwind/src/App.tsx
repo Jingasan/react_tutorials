c7d68768bec9750c978a6bfd872ed0aaa219544e
Jinga <xxx@gmail.com>
2024-08-10 23:23:32 +0900

    <add> initial commit

---
+++ 05_react_npm_packages/04_css/00_tailwind/src/App.tsx
@@ -0,0 +1,17 @@
+/**
+ * Tailwind CSS
+ * CSSフレームワーク
+ * https://tailwindcss.com/docs
+ */
+
+/**
+ * メインコンポーネント
+ * @returns
+ */
+const App: React.FC = () => {
+  return (
+    <p className="text-blue-600 font-bold text-5xl underline">Hello Tailwind</p>
+  );
+};
+
+export default App;
---
+++ 05_react_npm_packages/04_css/00_tailwind/src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  // StrictModeとは、アプリケーションの潜在的な問題点を洗い出すためのツールである。
+  // StrictModeにより、初回にレンダリングが２度行われるようになる。= useEffectは２回呼び出されるようになる。
+  // これにより、useEffectのアンマウント処理漏れを検出しやすくなる。
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
