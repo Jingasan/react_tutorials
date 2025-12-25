b182b949f971f528ae10ee1ed904769c30b03eb6
jinga <xxx@gmail.com>
2025-12-25 09:39:21 +0900

    <add> initial commit

---
+++ 05_react_npm_packages/20_react_select/src/App.tsx
@@ -0,0 +1,15 @@
+import SearchSelectBox from "./SearchSelectBox";
+import SearchMultiSelectBox from "./SearchMultiSelectBox";
+
+export default function App() {
+  return (
+    <div>
+      <div>
+        <SearchSelectBox />
+      </div>
+      <div>
+        <SearchMultiSelectBox />
+      </div>
+    </div>
+  );
+}
